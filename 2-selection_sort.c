c77acbb2d6af8933908600583df83eb2df23282f
peter <01peterkinyanjui@gmail.com>
2023-09-24 10:40:59 -0700

    task3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,27 +3,30 @@
 #include "sort.h"
 /**
  * selection_sort - sorts an array of integers in ascending
- * 		    order using the Selection sort algorithm
+ * order using the Selection sort algorithm
  * @array: array to be sorted
  * @size: size of array
- * Return: print the array after each time you swap two elements 
+ * Return: print the array after each time you swap two elements
  */
-void selection_sort(int *array, size_t size){
+void selection_sort(int *array, size_t size)
+{
 	size_t outer, min = 0, next;
 	size_t temp;
 
-	if(array == NULL || size < 2){
+	if (array == NULL || size < 2)
 		return;
-	}
-	
-	for(outer = 0; outer <= size - 1; outer++){
+	for (outer = 0; outer <= size - 1; outer++)
+	{
 		min = outer;
-		for(next = outer + 1; next < size; next++){
-			if(array[next] < array[min]){
+		for (next = outer + 1; next < size; next++)
+		{
+			if (array[next] < array[min])
+			{
 				min = next;
 			}
 		}
-		if(array[min] != array[outer]){
+		if (array[min] != array[outer])
+		{
 			temp = array[outer];
 			array[outer] = array[min];
 			array[min] = temp;
@@ -33,23 +36,21 @@ void selection_sort(int *array, size_t size){
 }
 /**
  * print_array - Prints an array of integers
- *
  * @array: The array to be printed
  * @size: Number of elements in @array
  */
 
 void myprint(const int *array, size_t size)
 {
-        size_t i;
+	size_t i;
 
-        i = 0;
-
-        while (array && i < size)
-        {
-                if (i > 0)
-                        printf(", ");
-                printf("%d", array[i]);
-                ++i;
-        }
-        printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
