c38804a26d0d6ba3b189329053c903b4aa5ec17f
peter <01peterkinyanjui@gmail.com>
2023-09-25 12:24:21 -0700

    task5

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -16,21 +16,21 @@ void cocktail_sort_list(listint_t **list);
  */
 void swap_next(listint_t **list, listint_t **end, listint_t **ptr_to_swapp)
 {
-    listint_t *temp = (*ptr_to_swapp)->next;
+	listint_t *temp = (*ptr_to_swapp)->next;
 
-    if ((*ptr_to_swapp)->prev != NULL)
-        (*ptr_to_swapp)->prev->next = temp;
-    else
-        *list = temp;
-    temp->prev = (*ptr_to_swapp)->prev;
-    (*ptr_to_swapp)->next = temp->next;
-    if (temp->next != NULL)
-        temp->next->prev = *ptr_to_swapp;
-    else
-        *end = *ptr_to_swapp;
-    (*ptr_to_swapp)->prev = temp;
-    temp->next = *ptr_to_swapp;
-    *ptr_to_swapp = temp;
+	if ((*ptr_to_swapp)->prev != NULL)
+		(*ptr_to_swapp)->prev->next = temp;
+	else
+		*list = temp;
+	temp->prev = (*ptr_to_swapp)->prev;
+	(*ptr_to_swapp)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *ptr_to_swapp;
+	else
+		*end = *ptr_to_swapp;
+	(*ptr_to_swapp)->prev = temp;
+	temp->next = *ptr_to_swapp;
+	*ptr_to_swapp = temp;
 }
 
 /**
@@ -42,21 +42,21 @@ void swap_next(listint_t **list, listint_t **end, listint_t **ptr_to_swapp)
  */
 void swap_prev(listint_t **list, listint_t **end, listint_t **ptr_to_swapp)
 {
-    listint_t *temp = (*ptr_to_swapp)->prev;
+	listint_t *temp = (*ptr_to_swapp)->prev;
 
-    if ((*ptr_to_swapp)->next != NULL)
-        (*ptr_to_swapp)->next->prev = temp;
-    else
-        *end = temp;
-    temp->next = (*ptr_to_swapp)->next;
-    (*ptr_to_swapp)->prev = temp->prev;
-    if (temp->prev != NULL)
-        temp->prev->next = *ptr_to_swapp;
-    else
-        *list = *ptr_to_swapp;
-    (*ptr_to_swapp)->next = temp;
-    temp->prev = *ptr_to_swapp;
-    *ptr_to_swapp = temp;
+	if ((*ptr_to_swapp)->next != NULL)
+		(*ptr_to_swapp)->next->prev = temp;
+	else
+		*end = temp;
+	temp->next = (*ptr_to_swapp)->next;
+	(*ptr_to_swapp)->prev = temp->prev;
+	if (temp->prev != NULL)
+		temp->prev->next = *ptr_to_swapp;
+	else
+		*list = *ptr_to_swapp;
+	(*ptr_to_swapp)->next = temp;
+	temp->prev = *ptr_to_swapp;
+	*ptr_to_swapp = temp;
 }
 
 /**
@@ -66,37 +66,37 @@ void swap_prev(listint_t **list, listint_t **end, listint_t **ptr_to_swapp)
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *end, *ptr_to_swapp;
-    bool shaken_not_stirred = false;
+	listint_t *end, *ptr_to_swapp;
+	bool shaken_not_stirred = false;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    for (end = *list; end->next != NULL;)
-        end = end->next;
+	for (end = *list; end->next != NULL;)
+		end = end->next;
 
-    while (shaken_not_stirred == false)
-    {
-        shaken_not_stirred = true;
-        for (ptr_to_swapp = *list; ptr_to_swapp != end; \
-			ptr_to_swapp = ptr_to_swapp->next)
-        {
-            if (ptr_to_swapp->n > ptr_to_swapp->next->n)
-            {
-                swap_next(list, &end, &ptr_to_swapp);
-                print_list((const listint_t *)*list);
-                shaken_not_stirred = false;
-            }
-        }
-        for (ptr_to_swapp = ptr_to_swapp->prev; ptr_to_swapp != *list; \
+	while (shaken_not_stirred == false)
+	{
+		shaken_not_stirred = true;
+		for (ptr_to_swapp = *list; ptr_to_swapp != end; \
+			       	ptr_to_swapp = ptr_to_swapp->next)
+		{
+			if (ptr_to_swapp->n > ptr_to_swapp->next->n)
+			{
+				swap_next(list, &end, &ptr_to_swapp);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+	for (ptr_to_swapp = ptr_to_swapp->prev; ptr_to_swapp != *list; \
 			ptr_to_swapp = ptr_to_swapp->prev)
-        {
-            if (ptr_to_swapp->n < ptr_to_swapp->prev->n)
-            {
-                swap_prev(list, &end, &ptr_to_swapp);
-                print_list((const listint_t *)*list);
-                shaken_not_stirred = false;
-            }
-        }
-    }
+	{
+		if (ptr_to_swapp->n < ptr_to_swapp->prev->n)
+		{
+			swap_prev(list, &end, &ptr_to_swapp);
+			print_list((const listint_t *)*list);
+			shaken_not_stirred = false;
+		}
+	}
+	}
 }
